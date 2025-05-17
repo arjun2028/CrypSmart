05befd863027d51d2d568cf975d7f4715b235505
Arjun Singh <arjun9650829@gmail.com>
2025-05-17 19:29:52 +0530

    Initial Commit

---
+++ CrypSmart/CrypSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypSmartApp.swift
+//  CrypSmart
+//
+//  Created by Arjun Singh on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CrypSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
