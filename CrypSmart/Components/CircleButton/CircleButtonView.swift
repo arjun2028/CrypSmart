4a87d12c003a240f65ef679a6a2c0656d2c9253c
Arjun Singh <arjun9650829@gmail.com>
2025-05-19 14:22:59 +0530

    Custom Home Header view build

---
+++ CrypSmart/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  CrypSmart
+//
+//  Created by Arjun Singh on 19/05/25.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
---
+++ CrypSmart/Extensions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  CrypSmart
+//
+//  Created by Arjun Singh on 19/05/25.
+//
+
+import Foundation
