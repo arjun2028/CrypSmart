67b3034fd8908e07923f99e1db9ed1f8008f974a
Arjun Singh <arjun9650829@gmail.com>
2025-05-19 19:38:54 +0530

    Coin row view setup

---
+++ CrypSmart/Core/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  CrypSmart
+//
+//  Created by Arjun Singh on 19/05/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
---
+++ CrypSmart/Extensions/PreviewProvider.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewProvider.swift
+//  CrypSmart
+//
+//  Created by Arjun Singh on 19/05/25.
+//
+
+import Foundation
